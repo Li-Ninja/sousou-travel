aae754a71359ddb124d4f7ff1b0fc58e582017ad
Ninja <lininja.tw@gmail.com>
2022-01-31 13:26:10 +0800

    feat: vue-router

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
+  <router-view />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// home script
+
+</script>
+
+<template>
+  <div>
+    home page
+  </div>
+</template>
