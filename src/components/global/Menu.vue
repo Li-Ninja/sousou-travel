2c7136f669f4ecf9804c9f6ab92de7a8c20f3814
Ninja <lininja.tw@gmail.com>
2022-02-06 18:08:59 +0800

    feat: menu button effect and change page function

--- src/components/global/Menu.vue
+++ src/components/global/Menu.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
 import { menuList } from '@/const/menu.const';
+import { MenuKeyEnum } from '@/enum/menu.enum';
+
+const route = useRoute();
+const router = useRouter();
+
+function toThePage(key: MenuKeyEnum) {
+  void router.push({ name: key });
+}
 
 </script>
 
@@ -16,13 +25,19 @@ import { menuList } from '@/const/menu.const';
       <div
         v-for="menu in menuList"
         :key="menu.key"
-        class="flex flex-col justify-center items-center bg-[#13545A] h-[144px] border-b border-[#5A8C8C]"
+        class="flex flex-col justify-center items-center group
+        bg-[#13545A] h-[144px] border-b border-[#5A8C8C] cursor-pointer"
       >
         <div
-          class="h-12 w-12 mb-2 bg-[#5a8886]"
+          class="h-12 w-12 mb-2 bg-[#5a8886] ease-out duration-300"
           :style="{maskImage: `url('${menu.icon}')`}"
+          :class="[menu.key === route.name ? 'bg-[#26A69A]' : 'group-hover:bg-[#498C8C] group-active:bg-[#26A69A]']"
+          @click="toThePage(menu.key)"
         />
-        <h2 class="text-[#5a8886] text-xl">
+        <h2
+          class="text-xl text-[#5a8886] ease-out duration-300"
+          :class="[menu.key === route.name ? 'text-[#26A69A]' : 'group-hover:text-[#498C8C] group-active:text-[#26A69A]']"
+        >
           {{ menu.name }}
         </h2>
       </div>
--- src/components/global/MobileMenu.vue
+++ src/components/global/MobileMenu.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
 import { mobileMenuList } from '@/const/menu.const';
+import { MenuKeyEnum } from '@/enum/menu.enum';
+
+const route = useRoute();
+const router = useRouter();
+
+function toThePage(key: MenuKeyEnum) {
+  void router.push({ name: key });
+}
 
 </script>
 
@@ -8,13 +17,19 @@ import { mobileMenuList } from '@/const/menu.const';
     <div
       v-for="menu in mobileMenuList"
       :key="menu.key"
-      class="flex flex-col grow justify-center items-center bg-[#13545A] h-[60px] border-b border-[#5A8C8C]"
+      class="flex flex-col grow justify-center items-center group
+      bg-[#13545A] h-[60px] border-b border-[#5A8C8C] cursor-pointer"
+      @click="toThePage(menu.key)"
     >
       <div
-        class="h-6 w-6 mb-1 bg-[#5a8886]"
+        class="h-6 w-6 mb-1 bg-[#5a8886] ease-out duration-300"
         :style="{maskImage: `url('${menu.icon}')`}"
+        :class="[menu.key === route.name ? 'bg-[#26A69A]' : 'group-hover:bg-[#498C8C] group-active:bg-[#26A69A]']"
       />
-      <h2 class="text-[#5a8886] text-xs">
+      <h2
+        class="text-[#5a8886] text-xs ease-out duration-300"
+        :class="[menu.key === route.name ? 'text-[#26A69A]' : 'group-hover:text-[#498C8C] group-active:text-[#26A69A]']"
+      >
         {{ menu.name }}
       </h2>
     </div>
