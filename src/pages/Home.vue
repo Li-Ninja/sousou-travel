7fad568ac2be348322f604a309faed5d1801bfd2
Ninja <lininja.tw@gmail.com>
2022-03-16 21:42:56 +0800

    fix: Home page background image height

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -58,9 +58,9 @@ function changeLanguage() {
     </div>
 
     <!-- background image -->
-    <div class="h-full absolute top-0 inset-x-0 bg-cover bg-center bg-no-repeat bg-black/50 -z-10" />
+    <div class="h-full fixed inset-0 bg-cover bg-center bg-no-repeat bg-black/50 -z-10" />
     <div
-      class="h-full absolute top-0 inset-x-0 bg-cover bg-center bg-no-repeat -z-20"
+      class="h-full fixed inset-0 bg-cover bg-center bg-no-repeat -z-20"
       :style="{backgroundImage: `url('src/assets/banner/1.jpg')`}"
     />
   </div>
